6f909086a83651059a1d688336a7834dd1f5b889
MuneerBangi <muneerbangi1@gmail.com>
2020-07-03 14:15:40 +0530

    Use Case 2

--- employeeWageComputation.sh
+++ employeeWageComputation.sh
@@ -1,7 +1,6 @@
 #Use Case 1
 
 #!/bin/bash -x
-
 isPresent=1
 randomcheck=$((RANDOM % 2))
 if [ $isPresent -eq $randomcheck ]
@@ -11,4 +10,19 @@ else
         echo "Employee is absent"
 fi
 
+#Use Case 2
+#!/bin/bash -x
+isPresent=1
+randomcheck=$((RANDOM % 2))
+if [ $isPresent -eq $randomcheck ]
+then
+        echo "Employee is present"
+        empRatePerHr=20;
+        empHrs=8;
+        salary=$(($empHrs*$empRatePerHr))
+        echo "salary=$salary"
+else
+        echo "Employee is absent"
+        echo "salary=0"
+fi
 
