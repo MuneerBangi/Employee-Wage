7a8012d619ed91fcd6c9aff16daad2604cd69058
MuneerBangi <muneerbangi1@gmail.com>
2020-07-03 16:47:43 +0530

    Use Case 3

--- employeeWageComputation.sh
+++ employeeWageComputation.sh
@@ -1,5 +1,4 @@
 #Use Case 1
-
 #!/bin/bash -x
 isPresent=1
 randomcheck=$((RANDOM % 2))
@@ -26,3 +25,18 @@ else
         echo "salary=0"
 fi
 
+#Use Case 3
+#!/bin/bash -x
+isPartTime=1
+isFulltime=2
+empRatePerHr=20;
+randomcheck=$((RANDOM % 2))
+if [ $isFulltime -eq $randomcheck ]
+	then
+   empHrs=8;
+	elif [ $isPartTime -eq $randomcheck ]
+		then
+   	empHrs=4;
+fi
+salary=$(($empHrs*$empRatePerHr))
+echo "salary=$salary"
